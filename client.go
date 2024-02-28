1114e9775d3bf6f5ced1dfc3c84c732dff070087
Myat Thiri Khin <137380130+myatthiri98@users.noreply.github.com>
2024-02-28 12:39:00 +0630

    chore : fix error
    chore : fix error

--- client.go
+++ client.go
@@ -5,5 +5,28 @@ import "github.com/gorilla/websocket"
 type client struct {
 	to     chan []byte
 	room   *room
-	socket websocket.Conn
+	socket *websocket.Conn
+}
+
+// where we extract the message from client
+func (c *client) ReadMessage() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+// where we send a message to client
+func (c *client) SendMessage() {
+	defer c.socket.Close()
+	for msg := range c.to {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			break
+		}
+	}
 }
